16a7cfdc62424151ac813cdc0a7f179bd28fe4b2
zakuArbor <nabimoon1234@gmail.com>
2016-12-22 23:04:30 -0500

    Debugging JSON problem

--- weather/script.js
+++ weather/script.js
@@ -7,6 +7,7 @@ if (navigator.geolocation) {
 		var api_link = "api.openweathermap.org/data/2.5/weather?lat={";
 		api_link += position.coords.latitude + "}&lon={" + position.coords.longitude + "}";
 		api_link += "&APPID={b5fa033436df259e1fe280df8a10aff6";
+		console.log(api_link);
 		$.getJSON(api_link, function(json) {
 			$("#data2").html(JSON.stringify(json));
 				var weather_info;
