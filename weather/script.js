f7f50d8bbe4133e1fe319129b74080ce2f488bae
zakuArbor <nabimoon1234@gmail.com>
2016-12-22 22:36:19 -0500

    Added api key

--- weather/script.js
+++ weather/script.js
@@ -3,7 +3,7 @@ if (navigator.geolocation) {
 		$("#data").html("latitude: " + position.coords.latitude + "<br>longitude: " + position.coords.longitude);
 	});
 }
-var weather_info = api.openweathermap.org/data/2.5/weather?lat={position,.coords.latitude}&lon={position.coords.longitude}; 
+var weather_info = api.openweathermap.org/data/2.5/weather?lat={position,.coords.latitude}&lon={position.coords.longitude}&APPID={b5fa033436df259e1fe280df8a10aff6}; 
 console.log(weather_info["name"]);
 //api key: b5fa033436df259e1fe280df8a10aff6
 //&APPID={b5fa033436df259e1fe280df8a10aff6} 
