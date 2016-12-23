5c7ea6ac0f6d1a997e3859b47b0f18c962b99a7f
zakuArbor <nabimoon1234@gmail.com>
2016-12-22 22:48:26 -0500

    fixed bug

--- weather/script.js
+++ weather/script.js
@@ -4,14 +4,15 @@
 if (navigator.geolocation) {
 	navigator.geolocation.getCurrentPosition(function(position) {
 		$("#data").html("latitude: " + position.coords.latitude + "<br>longitude: " + position.coords.longitude);
+		var api_link = "api.openweathermap.org/data/2.5/weather?lat={";
+		api_link += position.coords.latitude + "}&lon={" + position.coords.longitude + "}";
+		api_link += "&APPID={b5fa033436df259e1fe280df8a10aff6";
+		var weather_info = $.getJSON(api_link, function(json) {
+			$("#data").html(JSON.stringify(json));
+			}); 
+		console.log(weather_info["name"]);
 	});
 }
-var api_link = "api.openweathermap.org/data/2.5/weather?lat={";
-api_link += position.coords.latitude + "}&lon={" + position.coords.longitude + "}";
-api_link += "&APPID={b5fa033436df259e1fe280df8a10aff6";
-var weather_info = $.getJSON(api_link, function(json) {
-	$("#data").html(JSON.stringify(json));
-	}); 
-console.log(weather_info["name"]);
+
 //api key: b5fa033436df259e1fe280df8a10aff6
 //&APPID={b5fa033436df259e1fe280df8a10aff6} 
