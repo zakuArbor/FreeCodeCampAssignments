bd8f03c1f7f43a5cacf7dea189f25e177ed6d574
zakuArbor <nabimoon1234@gmail.com>
2016-12-22 23:02:08 -0500

    Testing JSON

--- weather/script.js
+++ weather/script.js
@@ -7,10 +7,14 @@ if (navigator.geolocation) {
 		var api_link = "api.openweathermap.org/data/2.5/weather?lat={";
 		api_link += position.coords.latitude + "}&lon={" + position.coords.longitude + "}";
 		api_link += "&APPID={b5fa033436df259e1fe280df8a10aff6";
-		var weather_info = $.getJSON(api_link, function(json) {
-			$("#data").html(JSON.stringify(json));
+		$.getJSON(api_link, function(json) {
+			$("#data2").html(JSON.stringify(json));
+				var weather_info;
+				var weather_info = json;
+
+				console.log(weather_info["name"]);
+
 			}); 
-		console.log(weather_info["name"]);
 	});
 }
 
