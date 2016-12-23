07b9888dd09b602d9885a3c61fd717d46cbf0543
zakuArbor <nabimoon1234@gmail.com>
2016-12-22 23:10:47 -0500

    Edited improper API link

--- weather/script.js
+++ weather/script.js
@@ -4,8 +4,8 @@
 if (navigator.geolocation) {
 	navigator.geolocation.getCurrentPosition(function(position) {
 		$("#data").html("latitude: " + position.coords.latitude + "<br>longitude: " + position.coords.longitude);
-		var api_link = "api.openweathermap.org/data/2.5/weather?lat={";
-		api_link += position.coords.latitude + "}&lon={" + position.coords.longitude + "}";
+		var api_link = "api.openweathermap.org/data/2.5/weather?lat=";
+		api_link += position.coords.latitude + "&lon=" + position.coords.longitude;
 		api_link += "&APPID={b5fa033436df259e1fe280df8a10aff6";
 		console.log(api_link);
 		$.getJSON(api_link, function(json) {
