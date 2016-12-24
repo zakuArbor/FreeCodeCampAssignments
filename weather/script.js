6dd3114643d0d6cf6ee0a601cd1c1c90b915644b
Ju Hong Kim <nabimoon1234@gmail.com>
2016-12-23 23:17:57 -0500

    Update script.js

--- weather/script.js
+++ weather/script.js
@@ -1,23 +1,20 @@
 
+$(document).ready(function () {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            $("#data").html("latitude: " + position.coords.latitude + "<br>long$
 
-
-if (navigator.geolocation) {
-	navigator.geolocation.getCurrentPosition(function(position) {
-		$("#data").html("latitude: " + position.coords.latitude + "<br>longitude: " + position.coords.longitude);
-		var api_link = "api.openweathermap.org/data/2.5/weather?lat=";
-		api_link += position.coords.latitude + "&lon=" + position.coords.longitude;
-		api_link += "&APPID=b5fa033436df259e1fe280df8a10aff6";
-		console.log(api_link);
-		$.getJSON(api_link, function(json) {
-			$("#data2").html(JSON.stringify(json));
-				var weather_info;
-				var weather_info = json;
-
-				console.log(JSON.stringify(json));
-
-			}); 
-	});
-}
-
-//api key: b5fa033436df259e1fe280df8a10aff6
-//&APPID={b5fa033436df259e1fe280df8a10aff6} 
+            var weather_link = "http://api.openweathermap.org/data/2.5/weather?$
+            weather_link += position.coords.latitude + "&lon=" + position.coord$
+            weather_link += "&APPID=b5fa033436df259e1fe280df8a10aff6";
+            //weather_link = "test.json";       
+            $.getJSON(weather_link, function(json) {
+                console.log(json);
+                var city = json["name"];
+                var weather_desc = json["weather"];
+                var temp = json["main"];
+                var country = json["country"];
+                console.log(weather_link);
+                console.log(city);
+            });
+        });
