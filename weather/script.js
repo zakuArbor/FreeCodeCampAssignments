3b2c7ac71786f7d9f712514a74b8ed85b50e2b3e
zakuArbor <nabimoon1234@gmail.com>
2016-12-23 23:54:13 -0500

    Fixed syntax errors

--- weather/script.js
+++ weather/script.js
@@ -2,10 +2,12 @@
 $(document).ready(function () {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function(position) {
-            $("#data").html("latitude: " + position.coords.latitude + "<br>long$
+            $("#data").html("latitude: " + position.coords.latitude + 
+			    "<br>longitude: " + position.coords.longitude);
 
-            var weather_link = "http://api.openweathermap.org/data/2.5/weather?$
-            weather_link += position.coords.latitude + "&lon=" + position.coord$
+            var weather_link = "http://api.openweathermap.org/data/2.5/weather";
+	    weather_link += "?lat=" + position.coords.latitude;
+            weather_link += "&lon=" + position.coord;
             weather_link += "&APPID=b5fa033436df259e1fe280df8a10aff6";
             //weather_link = "test.json";       
             $.getJSON(weather_link, function(json) {
@@ -18,3 +20,5 @@ $(document).ready(function () {
                 console.log(city);
             });
         });
+    }
+});
