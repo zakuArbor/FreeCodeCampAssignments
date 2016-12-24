49f97735cca82e04631db01b42075f44c8ce5123
zakuArbor <nabimoon1234@gmail.com>
2016-12-23 23:57:44 -0500

    Fixed weather link

--- weather/script.js
+++ weather/script.js
@@ -7,9 +7,8 @@ $(document).ready(function () {
 
             var weather_link = "http://api.openweathermap.org/data/2.5/weather";
 	    weather_link += "?lat=" + position.coords.latitude;
-            weather_link += "&lon=" + position.coord;
+            weather_link += "&lon=" + position.coords.longitude;
             weather_link += "&APPID=b5fa033436df259e1fe280df8a10aff6";
-            //weather_link = "test.json";       
             $.getJSON(weather_link, function(json) {
                 console.log(json);
                 var city = json["name"];
