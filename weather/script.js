d8061813bcbcaa74d9fd7a941fe7bba9fadfe392
zakuArbor <nabimoon1234@gmail.com>
2016-12-24 00:55:00 -0500

    gathered data

--- weather/script.js
+++ weather/script.js
@@ -1,9 +1,10 @@
+function celciusToFarenheit(temp) {
+    return temp * 9/5 + 32;
+}
 
 $(document).ready(function () {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function(position) {
-            $("#data").html("latitude: " + position.coords.latitude + 
-			    "<br>longitude: " + position.coords.longitude);
 
             var weather_link = "http://api.openweathermap.org/data/2.5/weather";
 	    weather_link += "?lat=" + position.coords.latitude;
@@ -13,10 +14,14 @@ $(document).ready(function () {
                 console.log(json);
                 var location = json["name"] + ", " + json["sys"]["country"];
                 var weather = json["weather"][0]["main"];
-                var temp = json["main"]["temp"];
+                var temp = json["main"]["temp"] -273.15; //calvin -273.15 = Celcius
 		var icon_id = json["weather"][0]["id"];
-                console.log(icon_id);
-            });
+		$("#temp").html(temp);
+		$("#weather").html(weather);
+		$("#icon").html();
+		$("#location").html(location);
+                console.log(celciusToFarenheit(temp));
+	    });	
         });
     }
 });
