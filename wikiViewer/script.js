429d7f07daa2de1ab1e7f5dcf2b27135b185829e
zakuArbor <nabimoon1234@gmail.com>
2017-01-04 18:10:37 -0500

    Rewrote url to gather more searches

--- wikiViewer/script.js
+++ wikiViewer/script.js
@@ -1,8 +1,15 @@
+function search_results(json) {
+
+}
+
 $(document).ready(function(){
 	$(document.getElementById("search_icon")).click(function() {
 		var search = document.getElementById("search_input").value;
-		var url = "https://en.wikipedia.org/w/api.php?action=";
-	url += "opensearch&search=" + search + "&profile=classic&format=json";
-	console.log(url);
+		var url = "https://en.wikipedia.org/w/api.php?format=json&action=";
+		//url += "opensearch&search=" + search + "&profile=classic";
+		url += "query&generator=search&gsrnamespace=0&gsrsearch=";
+		url += search + "&gsrlimit=10&prop=extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max";
+		console.log(url);
+		$.getJSON(url, search_results(json));
 	});
 });
