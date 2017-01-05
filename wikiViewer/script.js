21785607de8479ef2692694cf832a3ac7fea36de
zakuArbor <nabimoon1234@gmail.com>
2017-01-05 18:17:06 -0500

    Fixed Cors Problem - no 'Access-Control-Allow-Origin' header

--- wikiViewer/script.js
+++ wikiViewer/script.js
@@ -1,15 +1,15 @@
-function search_results(json) {
-
-}
-
 $(document).ready(function(){
 	$(document.getElementById("search_icon")).click(function() {
 		var search = document.getElementById("search_input").value;
 		var url = "https://en.wikipedia.org/w/api.php?format=json&action=";
 		//url += "opensearch&search=" + search + "&profile=classic";
 		url += "query&generator=search&gsrnamespace=0&gsrsearch=";
-		url += search + "&gsrlimit=10&prop=extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max";
+		url += search + "&gsrlimit=10&prop=extracts&pilimit=max&";
+		url += "exintro&explaintext&exsentences=1&exlimit=max";
+		url += "&callback=?";
 		console.log(url);
-		$.getJSON(url, search_results(json));
+		$.getJSON(url, function(json){
+			console.log(json);
+		});
 	});
 });
