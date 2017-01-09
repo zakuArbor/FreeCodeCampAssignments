09000ac3ddc5613c06938030ab7e4073d6fd7bb3
zakuArbor <nabimoon1234@gmail.com>
2017-01-08 21:54:25 -0500

    Displayed results to html (Working Product)

--- wikiViewer/script.js
+++ wikiViewer/script.js
@@ -8,14 +8,26 @@ $(document).ready(function(){
 		url += "exintro&explaintext&exsentences=1&exlimit=max";
 		url += "&callback=?";
 		$.getJSON(url, function(json){
+		 	var result_html = "";
+			console.log("test");
+                        $(document.getElementById("search_box")).css("top",0);
 			for (var key in json["query"]["pages"]) {
 			  console.log(key);
 			  var page_url = "https://en.wikipedia.org/?curid=";
 			  page_url += key;
 			  var title = json["query"]["pages"][key]["title"];
-			  var info = json["query"]["pages"][key]["extract"];
-			  console.log(title);			
+			  var content = json["query"]["pages"][key]["extract"];
+			  console.log(title);
+			  result_html += "<div id = 'result'>";
+			  result_html += "<a href = " + page_url  + ">";
+		  	  result_html += "<h3 id = 'result_title' class='bg-primary'>";
+			  result_html += title + "</h3>";
+	    	  	  result_html += "<div id = 'result_content'>";
+			  result_html += content + "</div>";
+			  result_html += "</a></div>";
 			}
+			$(document.getElementById("results")).html(result_html);
+			console.log(result_html);
 		});
 	});
 });
