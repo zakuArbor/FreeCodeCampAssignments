06f705a37c695825d4e7f22d20cdb4771290518e
zakuArbor <nabimoon1234@gmail.com>
2017-01-25 01:23:32 -0500

    Completed displaying all channels with basic layout

--- twitchTv/script.js
+++ twitchTv/script.js
@@ -7,6 +7,15 @@ function active(target, curr) {
   window.curr = target;
 }
 
+function generateHTML(channel_box, logo, channel, channel_url, status, state) {
+    var html = "<div class = 'status_box " + state + "'><a href = " + channel_url + ">";
+    html += "<img class = 'logo' src =" + logo + ">";
+    html += "<div class =  'status_content'>";
+    html += "<div class = 'name'>" + channel + "</div>";
+    html += "<div class = 'status'>" + status + "</div></div>";
+    $(channel_box).append(html);
+}
+
 function displayChannel(channel, channel_box) {
   var url = "https://wind-bow.gomix.me/twitch-api/streams/" + channel + "?callback=?";
   var html; 
@@ -20,6 +29,7 @@ function displayChannel(channel, channel_box) {
       channel_url = data.stream.channel["url"];
       logo = data.stream.channel.logo;
       status = data.stream.channel.status;
+      generateHTML(channel_box, logo, channel, channel_url, status, state);
     }
     else { //offline
       state = "offline";
@@ -28,28 +38,16 @@ function displayChannel(channel, channel_box) {
       $.getJSON(url, function(data) {
 	logo = data.logo;
         channel_url = data.url;
-        console.log(data);
+        console.log(data.logo);
+        generateHTML(channel_box, logo, channel, channel_url, status, state);
       });
     }
-    if (logo == undefined) {
-      logo = "";
-    }
-    console.log(logo);
-    var html = "<div class = 'status_box ";
-    html += state + "'>";
-    html += "<img class = 'logo' src = '";
-    html += logo + "'>"; 
-    html += "</div><div class = 'status_content'>";
-    html += "<div class = 'name'>" + channel;
-    html += "</div><div class = 'status'>" + status  + "</div></div>";
-    $(channel_box).append(html);
   });
 }
 
 function getChannelInfo() {
   var streamers = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
   var channel_box = document.getElementById("status");
-  console.log(channel_box);
   var html = "";
   for (i = 0; i < streamers.length; i++) {
     displayChannel(streamers[i], channel_box);
