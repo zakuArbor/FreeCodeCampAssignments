4a8b22f419b36af7c9ad8481571a93f1954adc86
zakuArbor <nabimoon1234@gmail.com>
2017-04-13 18:26:40 -0400

    Gather number input from 0-9

--- calculator/script.js
+++ calculator/script.js
@@ -1,15 +1,42 @@
 $(document).ready(function() {
 	var one = document.getElementById("one");
-	var two = document.getElementById("one");
-	var three = document.getElementById("one");
-	var four = document.getElementById("one");
-	var five = document.getElementById("one");
-	var six = document.getElementById("one");
-	var seven = document.getElementById("one");
-	var eight = document.getElementById("one");
-	var nine = document.getElementById("one");
-	var zero = document.getElementById("one");
+	var two = document.getElementById("two");
+	var three = document.getElementById("three");
+	var four = document.getElementById("four");
+	var five = document.getElementById("five");
+	var six = document.getElementById("six");
+	var seven = document.getElementById("seven");
+	var eight = document.getElementById("eight");
+	var nine = document.getElementById("nine");
+	var zero = document.getElementById("zero");
 	$(one).click(function() {
 		alert ($(one).text());
+	});
+	$(two).click(function() {
+		alert ($(two).text());
+	});
+	$(three).click(function() {
+		alert ($(three).text());
+	});
+	$(four).click(function() {
+		alert ($(four).text());
+	});
+	$(five).click(function() {
+		alert ($(five).text());
+	});
+	$(six).click(function() {
+		alert ($(six).text());
+	});
+	$(seven).click(function() {
+		alert ($(seven).text());
+	});
+	$(eight).click(function() {
+		alert ($(eight).text());
+	});
+	$(nine).click(function() {
+		alert ($(nine).text());
+	});
+	$(zero).click(function() {
+		alert ($(zero).text());
 	});
 });
