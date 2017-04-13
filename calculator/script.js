6b57a5b403489076c41a4b6c3d7205e0733e527c
zakuArbor <nabimoon1234@gmail.com>
2017-04-13 18:24:07 -0400

    Gather number input

---
+++ calculator/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+	var one = document.getElementById("one");
+	var two = document.getElementById("one");
+	var three = document.getElementById("one");
+	var four = document.getElementById("one");
+	var five = document.getElementById("one");
+	var six = document.getElementById("one");
+	var seven = document.getElementById("one");
+	var eight = document.getElementById("one");
+	var nine = document.getElementById("one");
+	var zero = document.getElementById("one");
+	$(one).click(function() {
+		alert ($(one).text());
+	});
+});
