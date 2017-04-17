98d71f6be410d1ee094a61f18f6a5463549ec086
zakuArbor <nabimoon1234@gmail.com>
2017-04-17 15:59:10 -0400

    Able to change break and study time amount

--- pomodoroClock/script.js
+++ pomodoroClock/script.js
@@ -1,7 +1,39 @@
-$(document).ready(function() {
-	var break_time = 0.1; //min
-	var study_time = 0.1; //min
+/**
+* @author	Ju Hong Kim <nabimoon1234@gmail.com or juhong.kim@mail.utoronto.ca>
+* @version	1
+*/
 
+/**
+* Returns either a 1 or 0 to decrement depending on the current time setting 
+*
+* Purpose: Want to make sure that time can never be less than or equal to 0
+*
+* @param time: the variable that holds the current time setting for either break or study time	
+* @return 1 iff time > 1 and 0 iff time = 1	 
+*/
+function decrease_time (time) {
+	if (time == 1) {
+		return 0;
+	}
+	return 1;
+}
+
+/**
+* Display updated time setting
+*
+* @param panel: The DOM reference to the display id
+* @param time: the variable that holds the current time setting for either break or study time
+**/
+function update_time_setting (panel, time) {
+	panel.innerHTML = time;
+}
+
+$(document).ready(function() {
+	var break_time = 1; //constraint: >= 1min
+	var study_time = 1; //constraint: >= 1min
+
+	/***************************************************************************/
+	//panel references
 	var increase_break_panel = document.getElementById("increase_break_time");
 	var increase_study_panel = document.getElementById("increase_study_time"); 
 	var decrease_break_panel = document.getElementById("decrease_break_time");
@@ -17,7 +49,38 @@ $(document).ready(function() {
 	var current_status = "Study" //Study or Break
 	var isStudyTime = true;
 	var start_new_clock = true;
+	/***************************************************************************/
+	/***************************************************************************/
+	//changing time intervals action listener
+	$(increase_study_panel).click(function() {
+		study_time += 1;
+		update_time_setting(display_study_setting, study_time);
+	});
 
+	$(increase_break_panel).click(function() {
+		break_time += 1;
+		update_time_setting(display_break_setting, break_time);
+	});
+
+	$(decrease_study_panel).click(function() {
+		study_time -= decrease_time(study_time);
+		update_time_setting(display_study_setting, study_time);
+	});
+
+	$(decrease_break_panel).click(function() {
+		break_time -= decrease_time(break_time);
+		update_time_setting(display_break_setting, break_time);
+	});
+	/***************************************************************************/
+	
+	/***************************************************************************/
+	var current_time = study_time * 60;
+	var current_status = "Study" //Study or Break
+	var isStudyTime = true;
+	var start_new_clock = true;
+	display_study_setting.innerHTML = study_time;
+	display_break_setting.innerHTML = break_time;
+	/***************************************************************************/
 
 	var time_interval = setInterval(function(){ 
 		if (start_new_clock) {
@@ -27,18 +90,12 @@ $(document).ready(function() {
 		current_time -= 1; //decrement by 1 second for each second that passes
 		var min = Math.floor((current_time/60) % 60); //min
 		var sec = Math.floor(current_time % 60);
-		console.log("start");
-		console.log(current_time);
 
 		current_time_panel.innerHTML = min + ":" + sec;
 
 		if (current_time <= 0) {
-			//clearInterval(time_interval);
-			console.log("switch");
-			console.log(current_time);
 			start_new_clock = true;
 			if (isStudyTime) {
-
 				current_time = break_time * 60;
 				isStudyTime = false;
 				current_status = "Break";	
