f054a50b593c1f3801b2a04fecb2bb1cbdd8b92b
zakuArbor <nabimoon1234@gmail.com>
2017-04-20 01:17:48 -0400

    Removed Progress Bar since it has too many bugs

--- pomodoroClock/script.js
+++ pomodoroClock/script.js
@@ -29,8 +29,8 @@ function update_time_setting (panel, time) {
 }
 
 $(document).ready(function() {
-	var break_time = 1; //constraint: >= 1min
-	var study_time = 1; //constraint: >= 1min
+	var break_time = 0.5; //constraint: >= 1min
+	var study_time = 0.6; //constraint: >= 1min
 
 	/***************************************************************************/
 	//panel references
@@ -41,7 +41,7 @@ $(document).ready(function() {
 
 	var display_break_setting_panel = document.getElementById("display_break_setting");
 	var display_study_setting_panel = document.getElementById("display_study_setting");
-
+	var display_clock_panel = document.getElementById("display_clock");
 	var current_status_panel = document.getElementById("current_status");
 	var current_time_panel = document.getElementById("current_time");
 	/***************************************************************************/
@@ -57,7 +57,6 @@ $(document).ready(function() {
 	var isStudyTime = true;
 	var start_new_clock = true;
 	var isPaused = true;
-	var wasPaused = false; //similar to isPause but is a boolean value if a pause did happened in the past and not the current time
 	var session_total_time = current_time; //total study or break time
 	var progress_time = 0; //% to fill in
 	display_study_setting.innerHTML = study_time;
@@ -65,32 +64,8 @@ $(document).ready(function() {
 	/***************************************************************************/
 	var min = Math.floor((current_time/60) % 60); //min
 	var sec = Math.floor(current_time % 60);
+	current_status_panel.innerHTML = current_status;
 	current_time_panel.innerHTML = min + ":" + sec;
-
-	var html = "<p>" + current_status + "</p><br>";
-	html += "<p>" + min + ":" + sec + "</p>";
-
-	var circle = new ProgressBar.Circle("#display_clock", {
-		strokeWidth: 4,
-		color: '#FCB03C',
-		//duration: current_time * 1000, //milliseconds
-		easing: 'easeInOut',
-		trailColor: '#F4F4F4',
-		trailWidth: 2,
-		svgStyle: null,
-		text: {
-    	autoStyleContainer: false,
-    	color: '#FCB03C'
-	    },
-	    from: { color: '#aaa', width: 1 },
-	    to: { color: '#333', width: 4 },
-	    // Set default step function for all animate calls
-	    step: function(state, circle) {
-	    		circle.path.setAttribute('stroke', state.color);
-	    		circle.path.setAttribute('stroke-width', state.width);
-	    		var value = Math.round(circle.value() * 100);
-	  }
-	});
 
 	/***************************************************************************/
 	//Action listener for changing break/study time setting and pausing clock
@@ -114,16 +89,12 @@ $(document).ready(function() {
 		update_time_setting(display_break_setting, break_time);
 	});
 
-	$(current_time_panel).click(function() {
+	$(display_clock_panel).click(function() {
 		if (isPaused) {
 			isPaused = false;
-			//circle.animate(1.0, current_time);
 		}
 		else {
-			console.log(progress_time);
 			isPaused = true;	
-			circle.animate(circle.value());
-			//circle.set(circle.value());
 		}
 	});
 	/***************************************************************************/
@@ -131,15 +102,12 @@ $(document).ready(function() {
 	var time_interval = setInterval(function(){
 		if (!isPaused) {	
 			if (start_new_clock) {
-				circle.setText(html);
-				start_new_clock = false;
-				circle.set(0.0);
-				//circle.animate(1.0, current_time * 1000);
-				session_total_time = current_time;
+				var min = Math.floor((current_time/60) % 60); //min
+				var sec = Math.floor(current_time % 60);
+				current_status_panel.innerHTML = current_status;
+				current_time_panel.innerHTML = min + ":" + sec;
 			}
-			current_time -= 0.5; //decrement by 1 second for each second that passes
-			//progress_time = (session_total_time - current_time)/session_total_time;
-			progress_time +=1;
+			current_time -= 1; //decrement by 1 second for each second that passes
 			var min = Math.floor((current_time/60) % 60); //min
 			var sec = Math.floor(current_time % 60);
 
@@ -157,14 +125,7 @@ $(document).ready(function() {
 					isStudyTime = true;
 					current_status = "Study";
 				} 
-			} 
+			}
 		}
 	}, 1000);
-
-	var draw_circle1 = setInterval(function(){
-		if (!isPaused) {		
-			current_time -= 0.5; //decrement by 1 second for each second that passes
-			circle.animate((session_total_time-current_time)/session_total_time, 0);
-		}
-	}, 500);
 });
