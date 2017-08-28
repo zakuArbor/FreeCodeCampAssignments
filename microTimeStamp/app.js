687d3e088f5836e2af56dc3196e6af31cf58fd8d
zakuArbor <nabimoon1234@gmail.com>
2017-08-27 23:34:44 -0400

    Create timestamp

--- microTimeStamp/app.js
+++ microTimeStamp/app.js
@@ -4,15 +4,35 @@ var path = require('path');
 
 var app = express();
 
-var logger = function (req, res, next) {
+/*var logger = function (req, res, next) {
 	console.log('Logging...');
 	next();
-}
+}*/
 
-app.use(logger);
+//app.use(logger);
 
-app.get('/', function (req, res) {
-	res.send('Hello World!');
+//view engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(body_parser.json());
+app.use(body_parser.urlencoded({extended: false}));
+
+//Set Static Path
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.get('/:date', function (req, res) {
+	var date = new Date(req.params.date);
+
+	var month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+	var timeStamp = {
+		unix: date.getTime(),
+		natural: month[date.getMonth()] + " " + date.getDay() + ',' + date.getFullYear(),
+	}
+	//res.render('index', timeStamp);
+	res.send(timeStamp);
 });	
 
 app.listen(8080, function() {
